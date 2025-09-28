1ea0476a3a42f26243e4dd59b26fec151670a7c8
User <edwardjoreilly01@gmail.com>
2025-09-28 21:10:31 +0100

    added jpa, created entity classes

---
+++ poc/src/main/java/station/poc/data/entity/ReadingEntity.java
@@ -0,0 +1,24 @@
+package station.poc.data.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "readings")
+@Getter
+@Setter
+public class ReadingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "sensor_id")
+    private Integer sensorId;
+    private String metric;
+    private LocalDateTime timestamp;
+    private Float value;
+}
---
+++ poc/src/main/java/station/poc/data/entity/SensorEntity.java
@@ -0,0 +1,22 @@
+package station.poc.data.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sensors")
+@Getter
+@Setter
+@AllArgsConstructor
+public class SensorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    public String name;
+    public String description;
+    public String metrics;
+}
